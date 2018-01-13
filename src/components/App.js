7c38b86a5943ce781352e108689f954d4e40a4a7
Claudio Acioli <kllaudyo@gmail.com>
2018-01-13 20:03:47 -0200

    feat: Adiciona rota e view Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
-import Menu from './Menu/Menu';
-import MenuItem from './Menu/MenuItem';
+import HomeView from "./HomeView";
 
 class App extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedIndex: null
-        }
-        this.onSelectIndex = this.onSelectIndex.bind(this);
-    }
-
-    onSelectIndex(index){
-        this.setState({selectedIndex:index});
-    }
-
     render() {
-        const { selectedIndex } = this.state;
         return (
-            <main className="App">
+            <div className="App">
                 <Header brand="alert('');"/>
-                <Menu>
-                    <MenuItem selectedIndex={selectedIndex} index="all" onSelect={this.onSelectIndex}>All</MenuItem>
-                    <MenuItem selectedIndex={selectedIndex} index="test" onSelect={this.onSelectIndex}>Test</MenuItem>
-                    <MenuItem selectedIndex={selectedIndex} index="another-test" onSelect={this.onSelectIndex}>Another Test</MenuItem>
-                </Menu>
-            </main>
+                <Route exact path="/" render={() => <HomeView />} />
+            </div>
         );
     }
 }
