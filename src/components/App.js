61402b49be84fb121e9f6663400bcb9587d8605e
Claudio Acioli <kllaudyo@gmail.com>
2018-01-13 19:16:51 -0200

    feat: Implementa versao básica componente Menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from "./Header";
+import Header from './Header';
+import Menu from './Menu/Menu';
+import MenuItem from './Menu/MenuItem';
 
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedIndex: null
+        }
+        this.onSelectIndex = this.onSelectIndex.bind(this);
+    }
+
+    onSelectIndex(index){
+        this.setState({selectedIndex:index});
+    }
+
     render() {
+        const { selectedIndex } = this.state;
         return (
-            <div className="App">
+            <main className="App">
                 <Header brand="alert('');"/>
-            </div>
+                <Menu>
+                    <MenuItem selectedIndex={selectedIndex} index="all" onSelect={this.onSelectIndex}>All</MenuItem>
+                    <MenuItem selectedIndex={selectedIndex} index="test" onSelect={this.onSelectIndex}>Test</MenuItem>
+                    <MenuItem selectedIndex={selectedIndex} index="another-test" onSelect={this.onSelectIndex}>Another Test</MenuItem>
+                </Menu>
+            </main>
         );
     }
 }
