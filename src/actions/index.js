1ef78788e31f9004c3c827c01c23f25e1910edca
Claudio Acioli <kllaudyo@gmail.com>
2018-01-13 20:54:15 -0200

    feat: Adiciona action creator initPosts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,13 @@
 export const
-    INIT_CATEGORIES = 'INIT_CATEGORIES';
+    INIT_CATEGORIES = 'INIT_CATEGORIES',
+    INIT_POSTS = 'INIT_POSTS';
 
-export const initCategories = ({categories}) => ({
-    type: INIT_CATEGORIES,
-    categories
-});
+export const
+    initCategories = ({categories}) => ({
+        type: INIT_CATEGORIES,
+        categories
+    }),
+    initPosts = ({posts}) => ({
+        type: INIT_POSTS,
+        posts
+    });
