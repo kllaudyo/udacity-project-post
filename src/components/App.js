3797c48ff36e511f41c4b0a52a21669986f8a676
Claudio Acioli <kllaudyo@gmail.com>
2018-01-13 17:34:30 -0200

    chore: Inicia repositório

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App"> alert(); </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('Verifica se renderiza sem erros!', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+});
