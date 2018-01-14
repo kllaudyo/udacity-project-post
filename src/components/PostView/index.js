51dc94d1699aa4e62899b92930384a97957e051f
Claudio Acioli <kllaudyo@gmail.com>
2018-01-14 12:22:50 -0200

    refactor: Implementa componente

--- src/components/PostView/index.js
+++ src/components/PostView/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import ArrowLeftIcon from 'react-icons/lib/fa/arrow-left';
 import ThumbsUpIcon from 'react-icons/lib/fa/thumbs-up';
 import Menu from '../Menu/Menu';
 import MenuItem from '../Menu/MenuItem';
 
-
-const PostView = props => (
-    <React.Fragment>
-        <Menu>
-            <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="back" ><ArrowLeftIcon color="white"/></MenuItem>
-            <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb" ><ThumbsUpIcon color="white" /></MenuItem>
-        </Menu>
-        <div className="container">
-            Ola
-        </div>
-    </React.Fragment>
-);
+//Todo: Linkar o componente para a API
+const PostView = ({post = {}, history}) => {
+    const {title = "", author = ""} = post;
+    return (
+        <React.Fragment>
+            <Menu>
+                <MenuItem onSelect={() => history.push('/') } selectedIndex={null} index="back" ><ArrowLeftIcon color="white"/></MenuItem>
+                <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb" ><ThumbsUpIcon color="white" /></MenuItem>
+            </Menu>
+            <div className="container post">
+                <h2>{title}</h2>
+            </div>
+        </React.Fragment>
+    )
+};
 
 PostView.propTypes = {
     id: PropTypes.string.isRequired
 };
 
-export default PostView;
+const mapStateToProps = ({posts}, ownProps) => ({
+    post : posts.filter(post => post.id === ownProps.id).shift(),
+    ownProps
+});
+
+export default withRouter(connect(mapStateToProps)(PostView));
