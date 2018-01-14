8fc0e7496b7677d5d28f2bfb4797cbce09a0f99a
Claudio Acioli <kllaudyo@gmail.com>
2018-01-13 23:14:36 -0200

    feat: Inscreve componete para ouvir a store

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 import Header from './Header';
-import HomeView from "./HomeView";
+import HomeView from './HomeView';
 import PostView from './PostView';
 
 class App extends Component {
-
     render() {
         return (
             <div className="App">
                 <Header brand="alert('');"/>
-
                 <Route exact path="/" render={() => <HomeView />} />
-
                 <Route path="/:id" render={({match}) => <PostView id={match.params.id} />} />
-
             </div>
         );
     }
 }
 
-export default App;
+export default connect()(App);
