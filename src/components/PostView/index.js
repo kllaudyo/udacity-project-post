4d178d9a25bfcaaac0b1b9d27c134b156ec9cc00
Claudio Acioli <kllaudyo@gmail.com>
2018-01-15 07:49:40 -0200

    refactor: Altera componente para prever estado e renderiza comentarios

--- src/components/PostView/index.js
+++ src/components/PostView/index.js
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import ArrowLeftIcon from 'react-icons/lib/fa/arrow-left';
 import ThumbsUpIcon from 'react-icons/lib/fa/thumbs-up';
 import ThumbsDownIcon from 'react-icons/lib/fa/thumbs-down';
+import { getCommentsByPost } from '../../utils/api';
 import Menu from '../Menu/Menu';
 import MenuItem from '../Menu/MenuItem';
+import Comment from '../Comment';
 
-const PostView = ({post = {}, history}) => {
-    const {title = "", body=""} = post;
-    return (
-        <React.Fragment>
-            <Menu>
-                <MenuItem onSelect={() => history.push('/') } selectedIndex={null} index="back" ><ArrowLeftIcon color="white"/></MenuItem>
-                <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb-up" ><ThumbsUpIcon color="white" /></MenuItem>
-                <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb-down" ><ThumbsDownIcon color="white" /></MenuItem>
-            </Menu>
-            <div className="container post">
-                <h2>{title}</h2>
-                <p>{body}</p>
-            </div>
-        </React.Fragment>
-    )
-};
+
+class PostView extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            comments : []
+        }
+    }
+
+    componentDidMount(){
+        const { id } = this.props;
+        getCommentsByPost(id)
+            .then(response => {
+                console.log(response);
+                this.setState({comments:response})
+            });
+    }
+
+    render(){
+        const {post = {}, history} = this.props;
+        const {title = "", body=""} = post;
+        return (
+            <React.Fragment>
+                <Menu>
+                    <MenuItem onSelect={() => history.push('/') } selectedIndex={null} index="back" ><ArrowLeftIcon color="white"/></MenuItem>
+                    <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb-up" ><ThumbsUpIcon color="white" /></MenuItem>
+                    <MenuItem onSelect={()=>alert('teste click')} selectedIndex={null} index="thumb-down" ><ThumbsDownIcon color="white" /></MenuItem>
+                </Menu>
+                <div className="container post">
+                    <h2>{title}</h2>
+                    <p>{body}</p>
+                    {this.state.comments.map(comment => (<Comment key={comment.id} text={comment.body} />))}
+                </div>
+            </React.Fragment>
+        );
+    }
+}
 
 PostView.propTypes = {
     id: PropTypes.string.isRequired
