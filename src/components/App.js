238ddf30c4c7b2ed6302822c8732b36b5be9f873
Claudio Acioli <kllaudyo@gmail.com>
2018-01-16 07:49:44 -0200

    feat: Implementa modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { withRouter, Route, Link } from 'react-router-dom';
+import { withRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Modal from 'react-modal';
 import { getAllCategories, getAllPosts } from '../utils/api';
 import { initCategories, initPosts } from '../actions';
 import './App.css';
@@ -8,8 +9,24 @@ import Header from './Header';
 import HomeView from './HomeView';
 import PostView from './PostView';
 
-
 class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            isAddModalOpen:false
+        };
+        this.openAddModal = this.closeAddModal.bind(this);
+        this.closeAddModal = this.closeAddModal.bind(this);
+    }
+
+    openAddModal(){
+        this.setState(()=> ({isAddModalOpen:true}));
+    }
+
+    closeAddModal(){
+        this.setState({isAddModalOpen:false});
+    }
 
     componentDidMount(){
 
@@ -29,13 +46,28 @@ class App extends Component {
     }
 
     render() {
+        const { isAddModalOpen } = this.state;
         return (
             <div className="App">
                 <Header brand="alert('Leitura');"/>
                 <Route exact path="/" render={() => <HomeView />} />
                 <Route exact path="/category/:path" render={({match}) => <HomeView path={match.params.path} />} />
                 <Route path="/post/:id" render={({match}) => <PostView id={match.params.id} />} />
-                <div className="fab-button-add"><Link to="/post/">Adicionar</Link></div>
+
+                <div className="fab-button-add"><button type="button" onClick={this.openAddModal}>Adicionar</button></div>
+
+                <Modal
+                    className="modal"
+                    overlayClassName="overlay"
+                    isOpen={isAddModalOpen}
+                    onRequestClose={this.closeAddModal}
+                    contentLabel='Modal'
+                >
+                    <div>
+                        <div>Maravilha</div>
+                    </div>
+                </Modal>
+
             </div>
         );
     }
